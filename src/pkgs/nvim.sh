e8425f3ae7c01d82288db7d37aaf8383077132fc
Cathal O'Grady <fierceinbattle@gmail.com>
2023-03-10 16:43:29 +0000

    now writing json data alongside download

--- src/pkgs/nvim.sh
+++ src/pkgs/nvim.sh
@@ -16,17 +16,24 @@ show_releases() {
 
 fetch() {
     : ${uri:=${default_uri}}
-    download_url=`http_get $uris \
-        | json_raw $gh_filter_downloads \
-        | grep ".*$uri$"`
+    json_response=`http_get $uris`
+    download_url=`json_print "$json_response" | json_raw $gh_filter_downloads | grep ".*$uri$"`
 
     download_file=`temp_file tgz`
     http_get_download $download_url -o $download_file
 
-    echo $download_file
+    printf "%s %s %s" "$download_file" "$download_url" "$json_response"
 }
 
+# | select (.browser_download_url | contains("'$uri'")))'
 installer() {
-    uri="$@"
-    extract $uri | tar xvf - -C $pkgdest
+    uri="$1"
+    download_url="$2"
+    shift 2
+    json_response="$@"
+    mkdir -p $pkgdest/nvim/
+    json_print "$json_response" \
+        | jq '.assets[] | select(.browser_download_url=="'$download_url'")' \
+        > $pkgdest/nvim/pk.json
+    extract $uri | tar xvf - -C $pkgdest/nvim/
 }
